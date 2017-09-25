3335bae1988f76d17007984d052229269775a5d7
Andrea Balducci <mtb.snowboard@gmail.com>
2017-09-25 16:45:51 +0200

    project setup

---
+++ src/IdentityServer.Host/Startup.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using IdentityModel;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using QuickstartIdentityServer;
+
+namespace IdentityServer.Host
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            // temp credential
+            RsaSecurityKey rsaSecurityKey = (RsaSecurityKey)null ?? new RsaSecurityKey(RSA.Create());
+            rsaSecurityKey.KeyId = CryptoRandom.CreateUniqueId(16);
+            SigningCredentials credential = new SigningCredentials((SecurityKey)rsaSecurityKey, "RS256");
+
+            // configure identity server with in-memory stores, keys, clients and scopes
+            services.AddIdentityServer()
+                .AddSigningCredential(credential)
+                .AddInMemoryIdentityResources(Config.GetIdentityResources())
+                .AddInMemoryApiResources(Config.GetApiResources())
+                .AddInMemoryClients(Config.GetClients())
+                .AddTestUsers(Config.GetUsers());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseIdentityServer();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
---
+++ src/IdentityServer.Host/Support/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+
+namespace IdentityServer.Host.Support
+{
+    public static class Extensions
+    {
+        public static async Task<ClaimsPrincipal> GetIdentityServerUserAsync(this HttpContext context)
+        {
+            var result = await context.AuthenticateAsync();
+            return result.Principal;
+        }
+    }
+}
